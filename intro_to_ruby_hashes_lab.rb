3cb449f69448ce6ff71e34265e1b0bcdde126dc3
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-13 18:06:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -63,9 +63,11 @@ monopoly = {
 	        mortgage_value => "$200"
 	      },
 	      b_and_o_railroad: {
-	        mortgage_value => ""
+	        mortgage_value => "$400"
 	      },
-	      shortline: {}
+	      shortline: {
+	        mortgage_value => "$800"
+	      }
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
